524277d7728fa0e8c79e5e238de1689aa4405587
Rivozn Coder <smtechofcmodz@outlook.com>
2026-01-30 00:54:02 +0300

    Add files via upload

--- setup/settings.js
+++ setup/settings.js
@@ -1,1 +1,22 @@
-
+module.exports = {
+  name: "Kayiza APIs",
+  description:
+    "Simple and easy to use",
+  key: "Solomon@19",
+  header: {
+    status: "Online!",
+    imageSrc: [
+      "https://vero-upload.zone.id/files/1767415004188_v3z6n0hkyw.gif",
+    ],
+    imageSize: {
+      mobile: "80%",
+      tablet: "40%",
+      desktop: "40%",
+    },
+  },
+  icon: "/docs/image/icon.png",
+  operator: "Ridz Coder",
+  telegram: "https://wa.link/243818786249",
+  messenger: "https://wa.link/243818786249",
+  github: "https://github.com/Ridz-coder01"
+};
